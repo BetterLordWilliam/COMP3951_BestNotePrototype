dc0ce18a289e9ceb93360c7b8250c9e9489b9f64
Bryson Lindy <bslindy@gmail.com>
2025-03-04 13:39:07 -0800

    file structure view implemented

--- ViewModels/FileStructureViewModel.cs
+++ ViewModels/FileStructureViewModel.cs
@@ -5,20 +5,36 @@ using BestNote_3951.Models;
 
 namespace BestNote_3951.ViewModels
 {
+    /// <summary>
+    /// Handles the logic for the file structure view.
+    /// 
+    /// Observable object just simplifies a buttload of boilerplater code so we can use the observer pattern out of the box easily.
+    /// </summary>
     public partial class FileStructureViewModel : ObservableObject
     {
+        /// <summary>
+        /// Files property is an ObservableCollection of BestFiles. ObservableCollection is part of the MVVM toolkit and it 
+        /// allows the View to automatically be notified when items are added/removed/updated.
+        /// </summary>
         public ObservableCollection<BestFile> Files { get; } = new ObservableCollection<BestFile>
         {
-            new BestFile { Name = "Document1.md", Path = "/docs/document1.md", FileType = "md" },
-            new BestFile { Name = "Document2.md", Path = "/docs/document2.md", FileType = "md" },
-            new BestFile { Name = "Presentation.pdf", Path = "/docs/presentation.pdf", FileType = "pdf" },
-            new BestFile { Name = "Image1.png", Path = "/images/image1.png", FileType = "png" }
+            new BestFile { Name = "comp3951notes.md", Path = "/docs/document1.md", FileType = "md" },
+            new BestFile { Name = "comp3721notes.md", Path = "/docs/document2.md", FileType = "md" },
+            new BestFile { Name = "textbook.pdf", Path = "/docs/presentation.pdf", FileType = "pdf" },
+            new BestFile { Name = "quakelogo.png", Path = "/images/image1.png", FileType = "png" }
         };
 
+        /// <summary>
+        /// Handles the open file logic (eventually)
+        /// 
+        /// The relay command attribute automatically generates a command property that can be invoked.
+        /// It basically just implements a Command  design pattern with a decorator instead of having to write that crap ourselves.
+        /// </summary>
+        /// <param name="file"></param>
         [RelayCommand]
         public void OpenFile(BestFile file)
         {
-            // TODO: Add logic to open the file.
+            // TODO: open file logic
         }
     }
 }
--- Views/FileStructureView.xaml.cs
+++ Views/FileStructureView.xaml.cs
@@ -2,6 +2,10 @@ using BestNote_3951.ViewModels;
 
 namespace BestNote_3951.Views
 {
+    /// <summary>
+    /// Connects the UI to it's corresponding ViewModel.
+    /// Sets the binding context to an instance of the ViewModel so the UI can access it
+    /// </summary>
     public partial class FileStructureView : ContentView
     {
         public FileStructureView()
@@ -10,10 +14,19 @@ namespace BestNote_3951.Views
             BindingContext = new FileStructureViewModel();
         }
 
+        /// <summary>
+        /// This is triggered whenever a file is selected in the CollectionView from FileStructureView.
+        /// 
+        /// It calls open file from the ViewModel to handle the file opening logic.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void CollectionView_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+            // makes sure we're selecting a BestFile
             if (e.CurrentSelection.FirstOrDefault() is Models.BestFile selectedFile)
             {
+                // passes the selected file to the OpenFile command
                 ((FileStructureViewModel)BindingContext).OpenFileCommand.Execute(selectedFile);
             }
         }
