0c1d7e9d3db97ccee24f50cc8df46640357d63d9
Olivia Grace <ograce2@my.bcit.ca>
2025-03-05 20:40:51 -0800

    Added trial license key for Syncfusion in App.xaml.cs

--- App.xaml.cs
+++ App.xaml.cs
@@ -4,6 +4,7 @@
     {
         public App()
         {
+            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Ngo9BigBOggjHTQxAR8/V1NMaF1cXmhKYVJ0WmFZfVtgdVRMYltbQHJPIiBoS35Rc0VgWXpcc3ZSQmRYV0d/");
             InitializeComponent();
         }
 
