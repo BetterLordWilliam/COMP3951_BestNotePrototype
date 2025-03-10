54b0e0e5c18fb837fbfa957ad23b278abf9e154d
Bryson Lindy <bslindy@gmail.com>
2025-03-09 22:52:00 -0700

    bound mdeditor and mdrenderer to the mainpanel so they are displayed in the UI

--- Views/MainPanel.xaml.cs
+++ Views/MainPanel.xaml.cs
@@ -2,8 +2,16 @@ namespace BestNote_3951.Views;
 
 public partial class MainPanel : ContentView
 {
+	public MarkdownEditorViewModel EditorViewModel { get; }
+	public MarkdownRendererViewModel RendererViewModel { get; }
 	public MainPanel()
 	{
 		InitializeComponent();
+
+		EditorViewModel = new MarkdownEditorViewModel();
+		RendererViewModel = new MarkdownRendererViewModel();
+
+		EditorView.BindingContext = EditorViewModel;
+		RendererView.BindingContext = RendererViewModel;
 	}
 }
