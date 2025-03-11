eebdef7108e3073385c7d04909ea081400e646b7
Bryson Lindy <bslindy@gmail.com>
2025-03-11 16:11:37 -0700

    added height/width request for markdown renderer webview

--- ViewModels/MarkdownRendererViewModel.cs
+++ ViewModels/MarkdownRendererViewModel.cs
@@ -13,7 +13,8 @@ namespace BestNote_3951
 
         public MarkdownRendererViewModel()
         {
-            string testConversion = Markdown.ToHtml("**Test Bold**");
+            var pipeline = new MarkdownPipelineBuilder().UseAdvancedExtensions().Build();
+            string testConversion = Markdown.ToHtml("**Test Bold**", pipeline);
             Debug.WriteLine($"markdig test: {testConversion}");
 
             WebViewSource = new HtmlWebViewSource
@@ -27,7 +28,7 @@ namespace BestNote_3951
                 Debug.WriteLine($"received the message: {text}");
 
                 // markdig the text 
-                var htmlBody = Markdown.ToHtml(text ?? "");
+                var htmlBody = Markdown.ToHtml(text, pipeline);
                 var html = $"<html><head><meta charset=\"utf-8\"></head><body>{htmlBody}</body></html>";
 
                 // force UI to update on hte main thread
