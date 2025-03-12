e7ec00e0d2c3d104e36b09b64b356d016fb93e71
Bryson Lindy <bslindy@gmail.com>
2025-03-12 13:33:34 -0700

    added comments to the markdown editor/renderer files

--- ViewModels/MarkdownRendererViewModel.cs
+++ ViewModels/MarkdownRendererViewModel.cs
@@ -4,9 +4,16 @@ using BestNote_3951.Messages;
 using Markdig;
 using Microsoft.Maui.Controls;
 
-/**
- * https://learn.microsoft.com/en-us/dotnet/maui/xaml/fundamentals/mvvm?view=net-maui-9.0
- */
+/*
+ * Sources:
+ * The data binding fundamentals were very helpful in getting data binding between the view and viewmodels
+* https://learn.microsoft.com/en-us/dotnet/maui/xaml/fundamentals/mvvm?view=net-maui-9.0
+* But not nearly as helpful as this WPF profressional going over the MVVM community toolkit for the
+* Avalonia framework. It really helped show how the Observable Properties and Objects should be connected
+* across views/viewmodels.
+* https://www.youtube.com/watch?v=bCryIp9HqIM
+*/
+
 namespace BestNote_3951
 {
     public partial class MarkdownRendererViewModel : ObservableObject
@@ -16,9 +23,8 @@ namespace BestNote_3951
 
         public MarkdownRendererViewModel()
         {
-            var pipeline = new MarkdownPipelineBuilder().UseAdvancedExtensions().Build();
-            string testConversion = Markdown.ToHtml("**Test Bold**", pipeline);
-            Debug.WriteLine($"markdig test: {testConversion}");
+            // pipeline gives access to advanced markdown rendering features.
+            var pipeline          = new MarkdownPipelineBuilder().UseAdvancedExtensions().Build();
 
             WebViewSource = new HtmlWebViewSource
             {
@@ -28,7 +34,7 @@ namespace BestNote_3951
             WeakReferenceMessenger.Default.Register<MarkdownTextChangedMessage>(this, (recipient, message) =>
             {         
                 string text = message.Value;
-                Debug.WriteLine($"received the message: {text}");
+                //Debug.WriteLine($"received the message: {text}");
 
                 // markdig the text 
                 var htmlBody = Markdown.ToHtml(text, pipeline);
@@ -38,7 +44,7 @@ namespace BestNote_3951
                 MainThread.BeginInvokeOnMainThread(() =>
                 {
                     WebViewSource = new HtmlWebViewSource { Html = html };
-                    Debug.WriteLine($"please please please: {WebViewSource.Html}");
+                    //Debug.WriteLine($"source HTML: {WebViewSource.Html}");
                 });
             });
         }
