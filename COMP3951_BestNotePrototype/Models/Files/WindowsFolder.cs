9bb1f5029463745c5ed563834bb2ea93342e0b5a
LordWilliam <willjordan05@icloud.com>
2025-03-19 16:54:54 -0700

    WO: Started Updating the structure for the models, implemented the delegation pattern for FileTreeItem and FolderTreeItem.

---
+++ COMP3951_BestNotePrototype/Models/Files/WindowsFolder.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BestNote_3951.Models.FileSystem;
+
+//public class BNFolder : ITreeViewItem, IBNParent
+//{
+    
+//}
