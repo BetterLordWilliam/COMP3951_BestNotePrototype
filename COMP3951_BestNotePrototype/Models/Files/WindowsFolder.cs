6935a700c0964942e891fbe18d0f7a96cc83eae8
LordWilliam <willjordan05@icloud.com>
2025-03-19 19:46:58 -0700

    WO: Modified the FileManagerServiec to just return fileinfo/directoryinfo objects. Working on updating XAML now./

--- COMP3951_BestNotePrototype/Models/Files/WindowsFolder.cs
+++ COMP3951_BestNotePrototype/Models/Files/WindowsFolder.cs
@@ -1,12 +1,31 @@
-﻿using System;
+﻿using BestNote_3951.Services;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace BestNote_3951.Models.FileSystem;
 
-//public class BNFolder : ITreeViewItem, IBNParent
-//{
-    
-//}
+
+public class WindowsFolder : IBNFolder
+{
+    private readonly FileManagerService _fileManagerService;
+    private DirectoryInfo directoryInfo;
+
+    public WindowsFolder(DirectoryInfo directoryInfo, FileManagerService fileManagerService)
+    {
+        _fileManagerService = fileManagerService;
+        this.directoryInfo = directoryInfo;
+    }
+
+    public DirectoryInfo DirectoryInfo
+    {
+        get => directoryInfo;
+        set => directoryInfo = value;
+    }
+
+    public ObservableCollection<ITreeViewItem> Children => throw new NotImplementedException();
+}
+
