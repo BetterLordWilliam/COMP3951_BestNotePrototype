ace254757bbe68fa39593852e8491165e95eb7f9
Bryson Lindy <bslindy@gmail.com>
2025-03-24 00:07:04 -0700

    hacked a way around the double print by putting a system timer on how quickly links can be inserted

--- COMP3951_BestNotePrototype/Views/MarkdownEditorView.xaml.cs
+++ COMP3951_BestNotePrototype/Views/MarkdownEditorView.xaml.cs
@@ -6,34 +6,37 @@ namespace BestNote_3951.Views;
 
 public partial class MarkdownEditorView : ContentView
 {
+    private string _previous;
+    private DateTime _coolDown;
+
 	public MarkdownEditorView()
 	{
 		InitializeComponent();
 
 		WeakReferenceMessenger.Default.Register<InsertAtCursorMessage>(this, (recipient, message) =>
 		{
-			InsertTextAtCursor(message.Value);
-		});
-	}
+            if (message.Value == _previous && (DateTime.UtcNow - _coolDown).TotalSeconds < 1)
+            {
+                return;
+            }
+            _previous = message.Value;
+            _coolDown = DateTime.UtcNow;
 
-	private void InsertTextAtCursor(string link)
-	{
-        string original = EditorControl.Text ?? string.Empty;
-        int cursorPosition = EditorControl.CursorPosition;
+            string original = EditorControl.Text ?? string.Empty;
+            int cursorPosition = EditorControl.CursorPosition;
 
-        if (cursorPosition < 0 || cursorPosition > original.Length)
-        {
-            cursorPosition = original.Length;
-        }
+            if (cursorPosition < 0 || cursorPosition > original.Length)
+            {
+                cursorPosition = original.Length;
+            }
 
-        string newText = original.Substring(0, cursorPosition) +
-            link + original.Substring(cursorPosition);
+            string newText = original.Substring(0, cursorPosition) +
+                message.Value + original.Substring(cursorPosition);
 
-		if (newText == original)
-		{
-			return;
-		}
+            EditorControl.Text = newText;
 
-        EditorControl.Text = newText;
+            EditorControl.CursorPosition = cursorPosition + message.Value.Length;
+
+        });
 	}
 }
