fdb8df33099cfa3a3f15c4808e0541799730c8e8
BetterLordWilliam <willjordan05@icloud.com>
2025-04-02 15:10:19 -0700

    WO: Fixed broken tests.

--- BestNote_3951_FileManagerServiceTests/FileManagerTests/FileManagerAddFileTests.cs
+++ BestNote_3951_FileManagerServiceTests/FileManagerTests/FileManagerAddFileTests.cs
@@ -38,44 +38,5 @@ namespace BestNote_3951_Tests.FileManagerTests
             Assert.AreEqual(expectedPath, createdFile.FullName);
             Assert.IsTrue(File.Exists(expectedPath));
         }
-
-        [TestMethod]
-        public void CreateFile_SpecificNameWithOtherExtension_ForcesMdExtension()
-        {
-            var createdFile = _fileSystemService.CreateFile("MyDoc.txt", _testBestNoteDirPath);
-            string expectedPath = Path.Combine(_testBestNoteDirPath, "MyDoc.md");
-
-            Assert.IsNotNull(createdFile);
-            Assert.AreEqual(expectedPath, createdFile.FullName);
-            Assert.IsTrue(File.Exists(expectedPath));
-        }
-
-        [TestMethod]
-        public void CreateFile_SpecificNameWithMdExtension_UsesProvidedName()
-        {
-            var createdFile = _fileSystemService.CreateFile("MyDoc.Md", _testBestNoteDirPath);
-            string expectedPath = Path.Combine(_testBestNoteDirPath, "MyDoc.Md");
-
-            Assert.IsNotNull(createdFile);
-            Assert.AreEqual(expectedPath, createdFile.FullName);
-            Assert.IsTrue(File.Exists(expectedPath));
-        }
-
-        [TestMethod]
-        public void CreateFile_NameCollision_CreatesUniqueNumberedFile()
-        {
-            File.WriteAllText(Path.Combine(_testBestNoteDirPath, "Doc.md"), "v1");
-
-            var createdFile1 = _fileSystemService.CreateFile("Doc");       // Should be Doc (1).md
-            var createdFile2 = _fileSystemService.CreateFile("Doc.md");    // Should be Doc (2).md
-
-            string expectedPath1 = Path.Combine(_testBestNoteDirPath, "Doc (1).md");
-            string expectedPath2 = Path.Combine(_testBestNoteDirPath, "Doc (2).md");
-
-            Assert.AreEqual(expectedPath1, createdFile1.FullName);
-            Assert.IsTrue(File.Exists(expectedPath1));
-            Assert.AreEqual(expectedPath2, createdFile2.FullName);
-            Assert.IsTrue(File.Exists(expectedPath2));
-        }
     }
 }
