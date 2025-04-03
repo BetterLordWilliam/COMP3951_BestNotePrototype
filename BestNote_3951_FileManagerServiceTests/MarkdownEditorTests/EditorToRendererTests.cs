3bf8cfa7ca6d19e779ddfc0dc045c54b68b35fcf
Bryson Lindy <bslindy@gmail.com>
2025-04-03 13:58:28 -0700

    editor to renderer tests

---
+++ BestNote_3951_FileManagerServiceTests/MarkdownEditorTests/EditorToRendererTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BestNote_3951.ViewModels;
+using BestNote_3951.Messages;
+using CommunityToolkit.Mvvm.Messaging;
+
+namespace BestNote_3951_Tests.MarkdownEditorTests
+{
+    [TestClass]
+    public class MarkdownEditorViewModelMessengerTests
+    {
+        [TestInitialize]
+        public void Setup()
+        {
+            WeakReferenceMessenger.Default.Reset();
+        }
+
+        [TestMethod]
+        public void ChangingMarkdownText_SendsMarkdownTextChangedMessage()
+        {
+            // Arrange
+            var vm = new MarkdownEditorViewModel();
+            string? receivedMarkdown = null;
+
+            WeakReferenceMessenger.Default.Register<MarkdownTextChangedMessage>(this,
+                (recipient, message) =>
+                {
+                    receivedMarkdown = message.Value;
+                });
+
+            // Act
+            vm.MarkdownText = "## A new heading for test";
+
+            // Assert
+            Assert.AreEqual("## A new heading for test", receivedMarkdown,
+                "Expected to receive the updated markdown text via MarkdownTextChangedMessage, but did not.");
+        }
+    }
+}
